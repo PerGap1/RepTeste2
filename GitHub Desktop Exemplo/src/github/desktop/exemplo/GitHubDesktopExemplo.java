db72e03df980f468bda9f34e11a4bdb116ff21a3
Gabriel <gabriel.galup@gmail.com>
2024-10-17 11:24:47 -0300

    Hello World

--- GitHub Desktop Exemplo/src/github/desktop/exemplo/GitHubDesktopExemplo.java
+++ GitHub Desktop Exemplo/src/github/desktop/exemplo/GitHubDesktopExemplo.java
@@ -1,11 +1,8 @@
-
 package github.desktop.exemplo;
-
 
 public class GitHubDesktopExemplo {
 
     public static void main(String[] args) {
-        
+        System.out.println("Hello World!");
     }
-
 }
